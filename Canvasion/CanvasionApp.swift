d030e7cfb5c80ddb51fd6518e2e7679142f5006b
Nike1421 <ommiemo54@gmail.com>
2024-02-25 16:23:17 -0800

    Initial Commit

---
+++ Canvasion/CanvasionApp.swift
@@ -0,0 +1,17 @@
+//
+//  CanvasionApp.swift
+//  Canvasion
+//
+//  Created by Om Dilip Naik on 2/25/24.
+//
+
+import SwiftUI
+
+@main
+struct CanvasionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
